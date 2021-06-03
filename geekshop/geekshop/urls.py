17c314dcaa4af57f62e3868163b65b3c35e4124d
HPC <mckena@mail.ru>
2021-06-03 22:59:07 +0300

    lesson_3

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mainapp import urls
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='main'),
     path('contact/', views.contact, name='contact'),
-    path('products/', include(urls), name='products'),
+    path('', views.index, name='main'),
+    path('products/', include('mainapp.urls', namespace ='products'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,17 +1,40 @@
 from django.shortcuts import render
+from .models import ProductCategory, Product
 
-def products(request):
+def products(request, pk=None):
     title = 'каталог'
-    links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+    categories = ProductCategory.objects.all()
+    links_menu = [{'href': '/products/', 'name': 'все'}]
+
+    # links_menu = [
+    #     {'href': 'category', 'name': 'все'},
+    #     {'href': '/products/1/', 'name': 'дом'},
+    #     {'href': '/products/2', 'name': 'офис'},
+    #     {'href': '/products/3', 'name': 'модерн'},
+    #     {'href': '/products/4', 'name': 'классика'},
+    # ]
+    for item in categories:
+        temp_dict = {}
+        temp_dict['href'] = f'/products/{item.pk}/'
+        temp_dict['name'] = f'{item.name}'
+        links_menu.append(temp_dict)
+
+
+
+    if pk:
+        category = ProductCategory.objects.get(pk=pk)
+        product = Product.objects.all()
+        context = {
+            'title': title,
+            'links_menu': links_menu,
+            'product': product,
+            'category': category,
+        }
+        return render(request, 'mainapp/category.html', context=context)
 
     context = {
         'title': title,
         'links_menu': links_menu,
     }
+
     return render(request, 'mainapp/products.html', context=context)
