c3d9d764f2ce4911e2af8cb7273f601a53b49ac7
HPC <mckena@mail.ru>
2021-06-12 22:52:27 +0300

    lesson_6

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,14 +1,18 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
+from django.template.loader import render_to_string
+
 from basketapp.models import Basket
 from mainapp.models import Product
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def basket(request):
     title = 'Корзина'
     basket = []
     if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
+        basket = Basket.objects.filter(user=request.user).order_by('product__category')
 
     context = {
         'basket': basket,
@@ -17,6 +21,7 @@ def basket(request):
     return render(request, 'basketapp/basket.html', context=context)
 
 
+@login_required
 def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
 
@@ -31,6 +36,32 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, pk):
-    content = {}
-    return render(request, 'basketapp/basket.html', content)
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, pk, quantity):
+    if request.is_ajax():
+        quantity = int(quantity)
+        new_basket_item = Basket.objects.get(pk=pk)
+
+        if quantity > 0:
+            new_basket_item.quantity = quantity
+            new_basket_item.save()
+        else:
+            new_basket_item.delete()
+
+        basket = Basket.objects.filter(user=request.user).order_by('product__category')
+
+        context = {
+            'basket': basket,
+        }
+
+        result = render_to_string('basketapp/includes/inc_basket_list.html', context)
+
+        return JsonResponse({"result": result})
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,15 +2,33 @@ from django.shortcuts import render, get_object_or_404
 
 from basketapp.models import Basket
 from .models import ProductCategory, Product
+import random
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample((list(products)), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+
+    return same_products
 
 
 def products(request, pk=None):
     title = 'каталог'
     links_menu = ProductCategory.objects.all()
 
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
+    basket = get_basket(request.user)
 
     if pk is not None:
         if pk == 0:
@@ -29,10 +47,29 @@ def products(request, pk=None):
         }
         return render(request, 'mainapp/category.html', context=context)
 
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+
     context = {
         'title': title,
         'links_menu': links_menu,
         'basket': basket,
+        'hot_product': hot_product,
+        'same_products': same_products,
     }
 
     return render(request, 'mainapp/products.html', context=context)
+
+
+def product(request, pk):
+    title = 'продукты'
+
+    content = {
+        'title': title,
+        'links_menu': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+
+    return render(request, 'mainapp/product.html', content)
+
